dfd6ea55e05b6044c9399f79a69e9fa6463cccfb
Nithya John <njohn2@rrc.ca>
2025-10-03 09:30:07 -0500

    feat: compute player_count inline and updated the output

--- Module-3-Assignment-2.py
+++ Module-3-Assignment-2.py
@@ -10,6 +10,16 @@ and a summry
 # Test data
 players = [("Sally", 95), ("Toby", 88), ("Sandeep", 10), ("Alice", 5)]
 
+# Feature: Length or Number of Players
+"""
+Use the list of tuples 
+Use the legth function to get the number of players
+"""
+
+player_count = len(players)
+
+
+
 # Expected Output
 print("=== Leaderboard ===")
 print(f"{None}")
@@ -18,7 +28,7 @@ print(f"{None}")
 
 
 print("=== Summary ===")
-print(f"Players: {None}")
+print(f"Players: {player_count}")
 print(f"Highest: {None}")
 print(f"Lowest:  {None}")
 print(f"Average: {None}")
