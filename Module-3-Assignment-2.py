f50e8eb19986fc3d2084fa6be5fea0b28cbb4f50
Nithya John <njohn2@rrc.ca>
2025-10-03 09:21:56 -0500

    feat: scaffold for the code with sample input and output template

---
+++ Module-3-Assignment-2.py
@@ -0,0 +1,25 @@
+"""
+Name: 
+Student ID: 
+Date: 
+Assignment 2 
+Build a code to get inputs from the user to create a Leaderboard 
+and a summry
+"""
+
+# Test data
+players = [("Sally", 95), ("Toby", 88), ("Sandeep", 10), ("Alice", 5)]
+
+# Expected Output
+print("=== Leaderboard ===")
+print(f"{None}")
+print(f"{None}")
+print(f"{None}")
+
+
+print("=== Summary ===")
+print(f"Players: {None}")
+print(f"Highest: {None}")
+print(f"Lowest:  {None}")
+print(f"Average: {None}")
+
